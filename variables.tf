33a41d721936fe6a777e2a1f9b9426d9cb505c5f
john24rel <72477831+john24rel@users.noreply.github.com>
2021-01-07 23:15:23 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -23,12 +23,6 @@ variable "instance_type" {
   type    = string
   default = "t2.micro"
 }
-variable "s3_bucket" {}
-variable "s3_folder_project" {}
-variable "s3_folder_region" {}
-variable "s3_folder_type" {}
-variable "s3_tfstate_file" {}
-}
 variable "ssh_port" {
   type    = string
   default = "22"
